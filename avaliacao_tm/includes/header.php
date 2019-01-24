8e75a2e7f3f57ffcc7253f4da96d93ce81bd602e
Carlos Teixeira <hello@carlosteixeira.xyz>
2019-01-24 10:02:47 +0000

    Add files via upload

---
+++ avaliacao_tm/includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML - hyper text markup language</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="h-left">
+            <h1>HTML <span>hyper text markup language</span></h1>
+        </div>
+        <div class="h-right">
+            <img src="media/menu.svg" alt="menu">
+        </div>
+    </header>
